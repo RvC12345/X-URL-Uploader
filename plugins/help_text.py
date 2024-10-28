bba6a0c804d2f85d2ae962921d8bf9ec01eb79aa
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-10-28 06:31:43 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -35,7 +35,7 @@ async def help_user(bot, update):
             text=Translation.HELP_USER,
             parse_mode="html",
             disable_web_page_preview=True,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
         )
 
 
@@ -46,5 +46,5 @@ async def start(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.START_TEXT.format(update.from_user.first_name),
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
         )
